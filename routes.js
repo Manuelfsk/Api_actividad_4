9fe0caf3049ee2cdcdcedf47ca0c43ccbb987278
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-12 11:05:02 -0500

    //15 iniciar el proceso de captura de datos

--- routes.js
+++ routes.js
@@ -4,5 +4,7 @@ let productosController = require("./API/controladores/productosController.js").
 
 //8 se crea la ruta tipo post con el tipo de ruta que se quiere especificar: ej. "/productos/save"
 app.post("/producto/save", function(request,response){
+// 15 iniciar proceso de captura de datos
+    productosController.save(request,response)
 
 })
