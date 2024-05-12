9f58eaeb475bb574658ef3eddbcf1909ddb05f69
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-11 20:57:42 -0500

    creacion del archivo configuraciones para ajustar todo lo referente a servidor, claves. etc

---
+++ config.js
@@ -0,0 +1,10 @@
+//3 crear vinculo de lectura entre los archovos en forma de objetos{}
+let config ={}
+
+//4 crear objeto .puerto para vincularlo al servidor creado
+config.puerto=3001
+
+
+
+//5 se crea el enlace exportar 
+module.exports.config=config
