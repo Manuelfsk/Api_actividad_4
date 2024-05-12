76351de312c590d5198dd6206c5556b6093f5051
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-11 20:57:05 -0500

    creacion de la carpeta API sub carpeta modelos

---
+++ API/modelos/productosModel.js
@@ -0,0 +1,15 @@
+//12 crear el objeto productosModel como variable
+let productosModel = {}
+
+
+
+
+
+
+
+
+
+
+
+//13 exportar el model productosModel
+module.exports.productosModel = productosModel
