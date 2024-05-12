ef605a5c99cd37103df5790f29ac324ea24505dc
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-11 20:56:13 -0500

    creacion de la carpeta API sub carpeta controladores

---
+++ API/controladores/productosController.js
@@ -0,0 +1,22 @@
+//14 importar productosModel para vincular conexion entre achivos
+let productosModel= require("../modelos/productosModel.js").productosModel
+//9 se crea el objeto{} productosController coomo variable
+let productosController={}
+//11 crear la conexion con el controllador .Save
+productosController.save = function(request,response){
+
+}
+
+
+
+
+
+
+
+
+
+
+//10 modelo de exportar prductoscontroller
+
+
+module.exports.productosController = productosController
