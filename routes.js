90f638dd489a8dc5d56ac65b12cbce9f1bcdf1cb
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-11 20:58:17 -0500

    se crea la ruta tipo post con el tipo de ruta que se quiere especificar: ej. "/productos/save"

---
+++ routes.js
@@ -0,0 +1,8 @@
+//11 importar comunicacion con productosController,js 
+let productosController = require("./API/controladores/productosController.js").productosController
+
+
+//8 se crea la ruta tipo post con el tipo de ruta que se quiere especificar: ej. "/productos/save"
+app.post("/producto/save", function(request,response){
+
+})
