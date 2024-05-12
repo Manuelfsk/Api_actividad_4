d3490bb82fc6bab36683caa5e9c3eb2b562f5b15
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-12 11:18:01 -0500

    //17 instalacion del paquete bodyParser

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // 1 cargar libreria express
 const express = require("express")
 global.app =express()
+
 //6. se crea el vinculo de comunicacion entre config y app.js
 global.config = require("./config.js").config
 
-//7 se crea la conunicacion entre el arcgivo app.js y routes.js
+//17 instalar la libreria body-parser
+let bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+//7 se crea la conunicacion entre el arcgcmdivo app.js y routes.js
 require("./routes.js")
 
 
