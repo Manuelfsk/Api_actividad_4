bdadaba37f0fe75a01e625b1a5297b55036ab2f2
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-13 10:50:52 -0500

    //21. creacion de las variables de busqueda en bases de datos categorias[] => "debe ser un código existente en la lista de categoría, no se acepta un código de categoría no existente"

--- API/modelos/productosModel.js
+++ API/modelos/productosModel.js
@@ -1,7 +1,7 @@
 //12 crear el objeto productosModel como variable
 let productosModel = {}
 
-
+//21. creacion de las variables de busqueda en bases de datos categorias[] => "debe ser un código existente en la lista de categoría, no se acepta un código de categoría no existente"
 
 
 
