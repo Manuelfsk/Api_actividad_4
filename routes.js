ce10623e33414f99886217b00a8cc64dfc26907a
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-13 19:40:45 -0500

    correccion en el post productos

--- routes.js
+++ routes.js
@@ -1,10 +1,17 @@
 //11 importar comunicacion con productosController,js 
 let productosController = require("./API/controladores/productosController.js").productosController
-
+//11.1 importar comunicacion con categoriasController,js
+let categoriasController = require("./API/controladores/categoriasController.js").categoriasController
 
 //8 se crea la ruta tipo post con el tipo de ruta que se quiere especificar: ej. "/productos/save"
-app.post("/producto/save", function(request,response){
+app.post("/productos/save", function(request,response){
 // 15 iniciar proceso de captura de datos
     productosController.save(request,response)
 
+})
+
+//8.1 se crea la ruta tipo post con el tipo de ruta que se quiere especificar: ej. "/categorias/save"
+app.post("/categorias/save", function(request, response){
+// 15.1 iniciar proceso de captura de datos
+    categoriasController.buscarcod_cat(request,response)
 })
