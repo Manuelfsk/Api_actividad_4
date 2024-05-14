6e9d2ca94a71882e10824a97a83be7df32979bec
Manuel Fernando Fonseca <fonsecafernando88@gmail.com>
2024-05-13 20:58:31 -0500

    se crearon las credenciales de busqueda en base de datos con su creacion de cod_prod

--- API/modelos/productosModel.js
+++ API/modelos/productosModel.js
@@ -1,12 +1,23 @@
+const { productosController } = require("../controladores/productosController")
+
 //12 crear el objeto productosModel como variable
 let productosModel = {}
 
 //21. creacion de las variables de busqueda en bases de datos categorias[] => "debe ser un código existente en la lista de categoría, no se acepta un código de categoría no existente"
 
+productosModel.busca_cod_cat = function(post, callback){
+    let ubicacion = categorias.findIndex((item) => item.cod_cat == post.cod_cat)
+    return callback({ ubicacion : ubicacion })
+}
+productosModel.buscarCod_prod =function(post, callback){
+    let buscar_prod = productos.findIndex((item) => item.cod_prod == post.cod_prod)
+    return callback ({ buscar_prod : buscar_prod })
+}
 
-
-
-
+productosModel.crearCod_prod = function(post, callback){
+    productos.push({ cod_prod: post.cod_prod, nombre: post.nombre })
+    return callback({ state: true})
+}
 
 
 
